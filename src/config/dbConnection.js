49633c57bbadbd0b6c5cdaf1879fbe3f5a559519
danishap07 <danishp.1999@gmail.com>
2024-01-05 18:12:28 +0530

    env_file_changes

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DBHOST,
     user: process.env.DBUSERNAME,
     password: process.env.DBPASSWORD,
     database: process.env.DATABASE,
-    port: 3306
+    port: process.env.DBPORT
 });
 
 
