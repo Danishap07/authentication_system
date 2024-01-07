ee77895132bcf8542c9141c1aadcbe77d66d01e3
danishap07 <danishp.1999@gmail.com>
2024-01-07 19:22:57 +0530

    1000

--- src/controllers/PortfolioContact/read.js
+++ src/controllers/PortfolioContact/read.js
@@ -1,7 +1,7 @@
 const dbConnection = require('../../config/dbConnection');
 
 const getContacts = (req, res) => {
-    const query = `SELECT * FROM contacted_mails`
+    const query = "SELECT `id`, `fullname`, `email`, `message` FROM `contacted_mails` WHERE 1 ORDER BY `id` DESC;";
     dbConnection.query(query, (err, result) => {
         try {
             return res.status(200).json({status: true, message: result});
